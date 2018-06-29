95d504591b07900201856e6fd968c682cabf7769
yangzhou <yzhou3_05@sina.com>
2018-06-29 14:46:12 +0800

    [zhouyang] #n/a add face detect program

---
+++ CMakeFiles/opencv_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_demo.dir/main.cpp.o"
+  "CMakeFiles/opencv_demo.dir/DisplayImage.cpp.o"
+  "opencv_demo.pdb"
+  "opencv_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
